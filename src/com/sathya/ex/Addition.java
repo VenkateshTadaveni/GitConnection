46c1f12f651ae8ed8dc3ff8e89a50f4a8f5a3246
VenkateshTadaveni <venkateshtadaveni@gmail.com>
2023-05-31 14:57:22 +0530

    addition

---
+++ src/com/sathya/ex/Addition.java
@@ -0,0 +1,13 @@
+package com.sathya.ex;
+
+public class Addition {
+
+	public static void main(String[] args) {
+	int num=100;
+	int num1=200;
+	int result=num+num1;
+	System.out.println(result);
+
+	}
+
+}
